9218c9104e47b44cd516159b3816147fe5667687
yexingxia <wangmingfei1984@yahoo.com.cn>
2015-12-23 16:14:47 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+/**
+ * thanks to https://github.com/defcc/rake-parser-less-common for the inspiration
+ */
+
+'use strict';
+
+var fs = require('fs');
+var root = fis.project.getProjectPath();
+var path = require('path');
+
+var currentModule = fis.config.get('namespace');
+var isCommonModule = currentModule != 'common';
+var commonModulePath = root;
+
+if ( isCommonModule ) {
+    commonModulePath = path.resolve( root, '../common' );
+}
+var lessLibImports = [];
+module.exports = function(content, file, conf){
+    if (conf.filedir.length && !lessLibImports.length) {
+        console.log('lib set\n');
+        conf.filedir.forEach(function( dir ){
+            var lessLib = path.resolve( commonModulePath, dir );
+            if (fis.util.isDir( lessLib )){
+                fs.readdirSync( lessLib ).forEach(function( item ){
+                    if ( item.indexOf('.less') > 0 ) {
+                        lessLibImports.push( fis.file( lessLib + '/' + item ).getContent() );
+                    }
+                });
+            } else if (fis.util.isFile( lessLib )){
+                lessLibImports.push( fis.file( lessLib ).getContent() );
+            }
+        });
+    }
+    // 先获取到 common 的模块依赖，添加到 less 文件中
+    return lessLibImports.join('') + content;
+};
